7fce04509b407ec89264db4cb3b78ee7fc63f19f
bbalasingam <47800674+bbalasingam@users.noreply.github.com>
2020-09-16 12:16:18 -0400

    Adding Lab3- Analytical and numerical solutions
    pdf + Handout

---
+++ Lab3_AnalyticalSol/Lab3-Handout.cpp
@@ -0,0 +1,25 @@
+//Declare your packages here
+
+
+//we will use a namespace, so we dont have to make a call to std:: for our methods
+
+
+//declare your variables as const qualified doubles
+
+int main(){
+
+	//declare the variables we will use for storage inside of our main function
+
+
+	//print the top of our table
+	
+	//declare a for loop and set the loop parameters.
+
+	//inside of the loop, set our logic that calculates the value at each iteration and prints it to the table.
+	return 0;
+
+}
+
+/*Once you have the code written, try playing with the step size and 
+max time and watch your computer speed through calculations.  Just be caeful you dont set
+the loop to high as your computer will calculate until it finishes!*/
